452dcff557a6a4db40e40eb9f0513a1345a46b59
David Rafe <david.rafe@gmail.com>
2021-08-23 16:11:40 -0400

    seeded with some dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user_admin = User.create(email: "dave@123.com", password: "123")
+
+art_post_1 = ArtPost.create(title: "ArtPost1", price: 50, description: "Beautiful", user_id: 1)
+art_post_2 = ArtPost.create(title: "ArtPost2", price: 75, description: "Beautiful", user_id: 1)
+art_post_3 = ArtPost.create(title: "ArtPost3", price: 100, description: "Beautiful", user_id: 1)
+
+art_post_1.image.attach(
+    io: File.open('./public/images/mr-tt-xb0wLfZH9Zo-unsplash.jpg'),
+    filename: 'mr-tt-xb0wLfZH9Zo-unsplash.jpg',
+    content_type: 'application/jpg'
+)
+
